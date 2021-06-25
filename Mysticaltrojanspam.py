a1cabd33156b6ec428cc7755e4542ebe216701e7
Mysticteam <86481049+Mysticteam@users.noreply.github.com>
2021-06-25 13:32:30 -0300

    Create Mysticaltrojanspam.py

---
+++ Mysticaltrojanspam.py
@@ -0,0 +1,9 @@
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/                                                   # Embedded file name: dg                                          import sys, os
+from time import sleep
+os.system('clear')
+logo = '\x1b[31;1m\n _______  ______  _____  _____ _______ __   _\x1b[32;1m     BY=🔥☠ⓂⓎⓈⓉⒾⒸⒶⓁ☠🔥1\x1b[31;1m\n    |    |_____/ |     |   |   |_____| | \\  |\x1b[32;1m     ᒪET'ᔕ EᒪIᗰIᑎᗩTE ᗩᒪᒪ ᗰYTᕼIᑕᗩᒪᒪY\x1b[31;1m\n\x1b[37;1m    |    |    \\_ |_____| __|   |     | |  \\_|\x1b[32;1m     V.1 private+\n                                             '                                                               print logo                                                        target = raw_input("\x1b[32;1mTarget's Number Phone\x1b[31;1m :\x1b[33;1m ")                                                        sleep(1)                                                          os.system('clear')
+print logo
+print '\x1b[32;1mTarget Found ! '                                 print '\x1b[31;1mStart Attack \x1b[33;1m' + target + '\x1b[31;1m ?'
+lanj = raw_input('\x1b[32;1mTab (y) to Continue\x1b[33;1m = ')
+if lanj == 'y':                                                       while True:
+        print '\x1b[32;1mSuccsses Send Trojan To\x1b[31;1m :\x1b[33;1m ' + target
