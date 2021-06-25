9deca8e014ffedaefcbfbf078a65a3bbe2a8f54d
Mysticteam <86481049+Mysticteam@users.noreply.github.com>
2021-06-25 13:29:05 -0300

    Create Mysticaltrojans.py

---
+++ Mysticaltrojans.py
@@ -0,0 +1,9 @@
+import sys, os
+from time import sleep
+os.system('clear')
+logo = '\x1b[31;1m\n _______  ______  _____  _____ _______ __   _\x1b[32;1m     By=🔥☠ⓂⓎⓈⓉⒾⒸⒶⓁ☠🔥\x1b[31;1m\n    |    |_____/ |     |   |   |_____| | \\  |\x1b[32;1m     ᒪET'ᔕ EᒪIᗰIᑎᗩTE ᗩᒪᒪ ᗰYTᕼIᑕᗩᒪᒪY\x1b[31;1m\n\x1b[37;1m    |    |    \\_ |_____| __|   |     | |  \\_|\x1b[32;1m     V.1 private+\n                                             '                                                               print logo                                                        target = raw_input("\x1b[32;1mTarget's Number Phone\x1b[31;1m :\x1b[33;1m ")                                                        sleep(1)                                                          os.system('clear')
+print logo
+print '\x1b[32;1mTarget Found ! '                                 print '\x1b[31;1mStart Attack \x1b[33;1m' + target + '\x1b[31;1m ?'
+lanj = raw_input('\x1b[32;1mTab (y) to Continue\x1b[33;1m = ')
+if lanj == 'y':                                                       while True:
+        print '\x1b[32;1mSuccsses Send Trojan To\x1b[31;1m :\x1b[33;1m ' + target
